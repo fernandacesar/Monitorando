e23b66a068e4fe5edb3163aa41a6ccb26674a52a
Kayane Rocha <kayannerch@gmail.com>
2019-06-10 13:12:15 -0300

    adicionando funcoes para abrir as paginas para cadastro e usuarios

--- PrototiposAltaFidel/scripts/main.js
+++ PrototiposAltaFidel/scripts/main.js
@@ -1,3 +1,11 @@
 function foco(elementID){
     document.getElementById(elementID).focus();    
 }
+
+function cadAluno(){
+    window.location.href = "cadAluno.html";
+}
+
+function cadProf(){
+    window.location.href = "cadProf.html";
+}
