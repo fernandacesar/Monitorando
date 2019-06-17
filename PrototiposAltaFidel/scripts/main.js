cc1a6007f1654bd9ccbd9e0429e67f881ed72cb9
Kayane Rocha <kayannerch@gmail.com>
2019-06-17 13:46:24 -0300

    adicionando mais campos de texto

--- PrototiposAltaFidel/scripts/main.js
+++ PrototiposAltaFidel/scripts/main.js
@@ -9,3 +9,43 @@ function cadAluno(){
 function cadProf(){
     window.location.href = "cadProf.html";
 }
+
+// $('input[name="aluProf"]').change(function(){
+//     if($('input[name="aluProf"]:checked').val() === "prof"){
+//         $('.mostrarEscon').show();
+//     }else{
+//         $('.mostrarEscon').hide();
+//     }
+// });
+
+// $('input[name="FlgPontua"]').change(function () {
+//     if ($('input[name="FlgPontua"]:checked').val() === "Sim") {
+//         $('.camposExtras').show();
+//     } else {
+//         $('.camposExtras').hide();
+//     }
+// });
+
+// function exibeProf(id){
+//     if(document.getElementById(id).style.display == "none"){
+//         document.getElementById(id).style.display = "inline";
+//     }else{
+//         document.getElementById(id).style.display = "none";
+//     }
+// }
+
+// function exibeAluno(id){
+//     if(document.getElementById(id).style.display == "none"){
+//         document.getElementById(id).style.display = "inline";
+//     }
+// }
+
+function exibirProf(){    
+    document.getElementById("dadosProf").style.display = "inline";
+    document.getElementById("dadosAluno").style.display = "none";
+}
+
+function exibirAluno(){
+    document.getElementById("dadosAluno").style.display = "inline";
+    document.getElementById("dadosProf").style.display = "none";
+}
