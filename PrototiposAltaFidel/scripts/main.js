a1d0fc402c959e8a1c18eb6c2948d7b8325d0738
Kayane Rocha <kayannerch@gmail.com>
2019-08-09 23:07:16 -0300

    Excluindo script que ocultava e mostrava campos

--- PrototiposAltaFidel/scripts/main.js
+++ PrototiposAltaFidel/scripts/main.js
@@ -1,21 +1,3 @@
 function foco(elementID){
     document.getElementById(elementID).focus();    
 }
-
-function cadAluno(){
-    window.location.href = "cadAluno.html";
-}
-
-function cadProf(){
-    window.location.href = "cadProf.html";
-}
-
-function exibirProf(){    
-    document.getElementById("dadosProf").style.display = "inline";
-    document.getElementById("dadosAluno").style.display = "none";
-}
-
-function exibirAluno(){
-    document.getElementById("dadosAluno").style.display = "inline";
-    document.getElementById("dadosProf").style.display = "none";
-}
