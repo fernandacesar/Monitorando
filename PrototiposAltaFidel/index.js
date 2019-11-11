c167b0225819619d8772a871e42b3d39450cc90c
Fernanda Cesar <fernandacesar24555@gmail.com>
2019-11-11 12:43:46 -0200

    Rotas

---
+++ PrototiposAltaFidel/index.js
@@ -0,0 +1,27 @@
+const express = require('express'); /*importando modulo express*/
+const handlebars = require('express-handlebars')
+const path = require('path');
+
+
+
+
+const server = express();
+
+server.use(express.static(path.join(__dirname,'/public')));
+server.engine('handlebars',handlebars({defaultLayout: 'main'}));
+server.set('view engine', 'handlebars');
+
+server.get('/', function(req, res){ 
+    res.render('home');
+});
+server.get('/servicos', function(req, res){ 
+    res.render('servico');
+});
+server.get('/contatos', function(req, res){ 
+    res.render('contato');
+});
+
+
+
+/*localhost:3000*/
+server.listen(3000); 
--- PrototiposAltaFidel/scripts/index.js
+++ PrototiposAltaFidel/scripts/index.js
@@ -59,6 +59,161 @@ server.get('/perfil/:id/alterarDados', function(req, res) {
         + '<input type="submit" value="Salvar">');
 });
 
+server.get('/perfil/:id/salvarAlteracao',function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Salvar alteração';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Salvar Alteração</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/solicitarLicenca',function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Solicitar Licença.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Solicitar Licença</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/solicitarLicenca/solicitar',function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Solicitar.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Solicitar</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+server.get('/perfil/:id/tornarseMonitor',function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Tornar-se Monitor.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Tornar-se Monitor</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/cadastrarMonitorias', function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Cadastrar Monitorias.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Cadastrar Monitorias</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/cadastrarMonitorias/cadastro', function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Cadatro Monitoria.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Cadatro Monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+
+
+server.get('/perfil/:id/cancelarMonitoria', function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Cancelar Monitoria.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Cancelar Monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/detalhesMonitoria', function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'detalhesMonitoria.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>detalhesMonitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/detalhesMonitoria/chat', function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Chat monitoria.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Chat monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/detalhesMonitoria/editar', function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Editar monitoria.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Editar monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/perfil/:id/detalhesMonitoria/cancelar', function(req,res){
+    const id = req.params.id;
+
+    let nome = '';
+    if(perfis[id] == undefined){
+        dados = 'Cancelar monitoria.';
+    }else{
+        dados = perfis[id].nome;
+    }
+
+    res.send('<h1>Cancelar monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
 server.get('/monitoriasInscritas/:idInscricao', function(req, res) {
     const usuarios = [];
     const monitorias = [];
@@ -87,9 +242,27 @@ server.get('/monitoriasInscritas/:idInscricao', function(req, res) {
 server.get('/monitorias', function(req, res) {
     res.send('<h1>Monitorias</h1>');
 });
+server.get('/monitorias/pesquisar', function(req,res){
+    
+    res.send('<h1>Pesquisar Monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
 
 server.get('/chat', function(req, res) {
     res.send('<h1>Chat de dúvidas</h1>');
+});
+
+
+server.get('/monitorias/pesquisar/verMais', function(req,res){
+    
+    res.send('<h1>Pesquisar Monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
+});
+
+server.get('/monitorias/pesquisar/verMais/inscrever', function(req,res){
+    
+    res.send('<h1>Pesquisar Monitoria</h1>'
+        + '<h3>' + dados +'</h3>');
 });
 
 server.listen(3000);
